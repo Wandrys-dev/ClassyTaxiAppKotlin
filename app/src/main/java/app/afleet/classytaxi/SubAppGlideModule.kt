77b4cbed6f21ec9066a16ffbadf7ff092924a68a
John <john.mcpherson@xtra.co.nz>
2022-08-26 19:00:16 +1200

    rename packages

---
+++ app/src/main/java/app/afleet/classytaxi/SubAppGlideModule.kt
@@ -0,0 +1,7 @@
+package app.afleet.classytaxi
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class SubAppGlideModule : AppGlideModule()
--- app/src/main/java/app/example/subscriptions/SubAppGlideModule.kt
+++
@@ -1,7 +0,0 @@
-package app.example.subscriptions
-
-import com.bumptech.glide.annotation.GlideModule
-import com.bumptech.glide.module.AppGlideModule
-
-@GlideModule
-class SubAppGlideModule : AppGlideModule()
